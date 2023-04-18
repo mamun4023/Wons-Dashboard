a94ed1333a6e4e699825ca89c528dbec0b53a28d
mamun4023 <mamun754023@gmail.com>
2023-04-18 14:48:20 +0600

    setup project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2 className="text-3xl font-bold">hello world</h2>
+      <h3>Hello worod</h3>
+      <h4>hello world</h4>
     </div>
   );
 }
-
-export default App;
